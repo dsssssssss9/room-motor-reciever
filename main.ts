4e17edd41d00985c2fc5e7802e5f6309e7e21fa0
dsssssssss9 <30810847+dsssssssss9@users.noreply.github.com>
2021-08-16 19:44:00 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,19 +1,53 @@
+/**
+ * RobotBit with in-built Neopixel driver
+ * 
+ * Radio group set to 80
+ * 
+ * RobotBit led on pin16
+ * 
+ * DC motor connected to M1B
+ * 
+ * 2 servos connected to S1 & S8
+ * 
+ * Number received meaning : 
+ * 
+ * 0 = stop all DC motors + set S1 & S8 to 90°
+ * 
+ * 1 = Set DC motor to speed of 64
+ * 
+ * 2 = set S! + S8 to random posistions
+ * 
+ * Use A / B / A+B to test 
+ * 
+ *  motor / servo manually
+ */
 radio.onReceivedNumber(function (receivedNumber) {
     if (receivedNumber == 0) {
         robotbit.MotorStopAll()
+        robotbit.Servo(robotbit.Servos.S1, 90)
+        robotbit.Servo(robotbit.Servos.S8, 90)
     } else if (receivedNumber == 1) {
         robotbit.MotorRun(robotbit.Motors.M1B, 64)
+    } else if (receivedNumber == 2) {
+        robotbit.Servo(robotbit.Servos.S1, randint(20, 160))
+        robotbit.Servo(robotbit.Servos.S8, randint(20, 160))
     }
 })
+// Set S1 + S8 to random posistions
+// 
 input.onButtonPressed(Button.A, function () {
     robotbit.Servo(robotbit.Servos.S1, randint(20, 160))
     robotbit.Servo(robotbit.Servos.S8, randint(10, 175))
 })
+// Stop DC motor on M1B
+// 
+// Set S1 & S8 to 90°
 input.onButtonPressed(Button.AB, function () {
     robotbit.Servo(robotbit.Servos.S8, 90)
     robotbit.Servo(robotbit.Servos.S1, 90)
     robotbit.MotorStopAll()
 })
+// Set M1B to s[eed of 64
 input.onButtonPressed(Button.B, function () {
     robotbit.MotorRun(robotbit.Motors.M1B, 64)
 })
