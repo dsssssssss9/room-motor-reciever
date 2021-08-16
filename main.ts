21da576dd83c04c4209b97b3dd0a3022db96e676
dsssssssss9 <30810847+dsssssssss9@users.noreply.github.com>
2021-08-16 19:44:51 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,26 +1,3 @@
-/**
- * RobotBit with in-built Neopixel driver
- * 
- * Radio group set to 80
- * 
- * RobotBit led on pin16
- * 
- * DC motor connected to M1B
- * 
- * 2 servos connected to S1 & S8
- * 
- * Number received meaning : 
- * 
- * 0 = stop all DC motors + set S1 & S8 to 90°
- * 
- * 1 = Set DC motor to speed of 64
- * 
- * 2 = set S! + S8 to random posistions
- * 
- * Use A / B / A+B to test 
- * 
- *  motor / servo manually
- */
 radio.onReceivedNumber(function (receivedNumber) {
     if (receivedNumber == 0) {
         robotbit.MotorStopAll()
@@ -51,9 +28,32 @@ input.onButtonPressed(Button.AB, function () {
 input.onButtonPressed(Button.B, function () {
     robotbit.MotorRun(robotbit.Motors.M1B, 64)
 })
+/**
+ * RobotBit with in-built Neopixel driver
+ * 
+ * Radio group set to 80
+ * 
+ * RobotBit led on pin16
+ * 
+ * DC motor connected to M1B
+ * 
+ * 2 servos connected to S1 & S8
+ * 
+ * Number received meaning : 
+ * 
+ * 0 = stop all DC motors + set S1 & S8 to 90°
+ * 
+ * 1 = Set DC motor to speed of 64
+ * 
+ * 2 = set S! + S8 to random posistions
+ * 
+ * Use A / B / A+B to test 
+ * 
+ *  motor / servo manually
+ */
 radio.setGroup(80)
 let strip = neopixel.create(DigitalPin.P16, 4, NeoPixelMode.RGB)
 strip.setBrightness(24)
 strip.clear()
-strip.showColor(neopixel.colors(NeoPixelColors.Green))
+strip.showColor(neopixel.colors(NeoPixelColors.Blue))
 robotbit.MotorStopAll()
